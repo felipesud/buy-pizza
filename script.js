8c146f84ddffc6bb834aa99d168954cd7a7176e5
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-11-05 10:41:04 -0300

    Edita  Modal

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ const cs = (el)=>document.querySelectorAll(el);
 //Mapear as pizzas
 pizzaJson.map((item, index)=> {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
     // preencher as informações em pizza-item
+    pizzaItem.setAttribute('data-key', index);
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
 
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = 'R$ ${item.price.toFixed(2)}';
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
       e.preventDefault();
+      let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+      c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+      c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+     c('.pizzaInfo--actualPrice').innerHTML = pizzaJson[key].price.toFixed(2);
 
       c('.pizzaWindowArea').style.opacity = 0;
       c('.pizzaWindowArea').style.display = 'flex';
