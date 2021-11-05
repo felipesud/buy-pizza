1fdb2c2d33e73eb8b8c5aa2f2cf78a4afbf7e312
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-11-05 16:31:30 -0300

    Add modalQt default Qtt

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let modalQt = 1;
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
@@ -15,6 +16,7 @@ pizzaJson.map((item, index) => {
   pizzaItem.querySelector('a').addEventListener('click', (e) => {
     e.preventDefault();
     let key = e.target.closest('.pizza-item').getAttribute('data-key');
+    modalQt = 1;
     c('.pizzaBig img').src = pizzaJson[key].img;
     c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
     c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
@@ -26,6 +28,8 @@ pizzaJson.map((item, index) => {
       }   
       size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
     });
+
+    c('.pizzaInfo--qt').innerHTML = modalQt;
 
     c('.pizzaWindowArea').style.opacity = 0;
     c('.pizzaWindowArea').style.display = 'flex';
